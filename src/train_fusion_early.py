65d7a21f6a75cf0fbd98cbc58053826f92b37f3c
rd935 <rd935@scarletmail.rutgers.edu>
2025-12-04 00:20:36 -0500

    Tweak to Core

--- src/train_fusion_early.py
+++ src/train_fusion_early.py
@@ -213,7 +213,7 @@ def main():
     ).to(DEVICE)
 
     criterion = nn.CrossEntropyLoss(weight=weights)
-    optimizer = Adam(model.parameters(), lr=lr, weight_decay=1e-4)
+    optimizer = Adam(model.parameters(), lr=lr, weight_decay=5e-4)
 
     best_val_acc = 0.0
     best_epoch = 0
